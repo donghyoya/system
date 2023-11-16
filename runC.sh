39ae85d67b19b5e3dfd8a25b25dafa21bc005d8b
donghyoya <backjui@kookmin.ac.kr>
2023-11-16 18:01:54 +0900

    commit test c

---
+++ runC.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# 첫 번째 매개변수로 받은 파일 이름
+FILENAME=$1
+
+# 파일 확장자 없이 파일 이름만 추출
+BASENAME=$(basename "$FILENAME" .c)
+
+# 컴파일
+gcc "$FILENAME" -o "$BASENAME"
+
+# 실행 파일이 성공적으로 생성되었는지 확인
+if [ -f "./$BASENAME" ]; then
+    echo "Running $BASENAME..."
+    ./"$BASENAME"
+else
+    echo "Compilation failed."
+fi
+
