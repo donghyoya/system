4b876a2d1dd4d8c104f9df6fc4ae4a65caa0a444
unknown <dongwooya123@gamil.com>
2023-11-20 17:35:33 +0900

    fix client code

--- testsocket/tcp_client.c
+++ testsocket/tcp_client.c
@@ -21,7 +21,7 @@ int main(){
 
     //서버 주소 설정
     serv_addr.sin_family = AF_INET;
-    serve_addr.sin_port = htons(PORT);
+    serv_addr.sin_port = htons(PORT);
 
     // 서버 IP 주소 설정
     if(inet_pton(AF_INET, "127.0.0.1", &serve_addr.sin_addr) <= 0){
